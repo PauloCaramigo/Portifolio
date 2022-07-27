3dba97478dddf908a08183ebd750b58b4f40d495
unknown <paulo.domingues.caramigo@gmail.com>
2022-07-27 17:13:21 -0300

    Funcionamento da calculadora por JS, exibindo o resultado por console

---
+++ javascript/calculator.js
@@ -0,0 +1,44 @@
+function sum() {
+    let num = document.querySelector("#numberOne").value;
+    num = +num;
+    let num2 = document.querySelector("#numberTwo").value;
+    num2 = +num2;
+
+    console.log(num + num2);
+}
+
+function subtraction() {
+    let num = document.querySelector("#numberOne").value;
+    num = +num;
+    let num2 = document.querySelector("#numberTwo").value;
+    num2 = +num2;
+
+    console.log(num - num2);
+}
+
+function multiplication() {
+    let num = document.querySelector("#numberOne").value;
+    num = +num;
+    let num2 = document.querySelector("#numberTwo").value;
+    num2 = +num2;
+
+    console.log(num * num2);
+}
+
+function division() {
+    let num = document.querySelector("#numberOne").value;
+    num = +num;
+    let num2 = document.querySelector("#numberTwo").value;
+    num2 = +num2;
+
+    console.log(num / num2);
+}
+
+function pow() {
+    let num = document.querySelector("#numberOne").value;
+    num = +num;
+    let num2 = document.querySelector("#numberTwo").value;
+    num2 = +num2;
+
+    console.log(Math.pow(num, num2));
+}
